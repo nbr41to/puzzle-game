33d58a118ec2b4e58d2d8dc583248aca6defcc9b
nbr41to <nbr.41to@gmail.com>
2021-07-21 17:02:25 +0900

    むりぽ

--- src/Board.jsx
+++ src/Board.jsx
@@ -27,19 +27,51 @@ export const Board = () => {
     setDrops(initialDrops);
   }, []);
 
-  const changeDrop = (moveId, id) => {};
-
+  const changeDrop = (dragId, targetId) => {
+    if (!targetId || !dragId) return;
+    // const drag = drops.find((drop) => {
+    //   return drop.position.x === 0 && drop.position.y === 0;
+    // });
+    // const target = drops.find(
+    //   (drop) => drop.position.x === 0 && drop.position.y === 1
+    // );
+    const drag = drops.find((drop) => drop.id === dragId);
+    const target = drops.find((drop) => drop.id === targetId);
+    const newDrops = drops.map((drop) => {
+      if (drop.id === drag?.id) {
+        const newDrop = { ...drop, position: target.position };
+        console.log(drop);
+        console.log(newDrop);
+        return newDrop;
+      }
+      if (drop.id === target?.id) {
+        const newDrop = { ...drop, position: drag.position };
+        console.log(drop);
+        console.log(newDrop);
+        return newDrop;
+      }
+      return drop;
+    });
+    setDrops(newDrops);
+  };
+  console.log(drops);
   return (
     <div className='board'>
       {[0, 1, 2, 3, 4].map((i) => (
-        <div className='flex'>
+        <div className='flex' key={i}>
           {drops
             .filter((drop) => drop.position.y === i)
+            .sort((a, b) => {
+              if (a.position.x > b.position.x) return 1;
+              if (a.position.x < b.position.x) return -1;
+              return 0;
+            })
             .map((drop) => (
-              <Drop drop={drop} />
+              <Drop drop={drop} changeDrop={changeDrop} key={drop.id} />
             ))}
         </div>
       ))}
+      <button onClick={changeDrop}>button</button>
     </div>
   );
 };
--- src/Drop.jsx
+++ src/Drop.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import { useGesture } from 'react-use-gesture';
 import { useSpring, animated } from 'react-spring';
-export const Drop = ({ drop }) => {
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
-  const bind = useGesture({
-    onDrag: ({ down, movement: [mx, my] }) => {
-      api.start({ x: down ? mx : 0, y: down ? my : 0 });
-    },
-    onDragEnd: (state) => {
-      console.log(state.previous);
-      const el = document.elementFromPoint(
-        state.previous[0],
-        state.previous[1]
-      );
-      console.log(el);
-    },
-  });
-  console.log(x, y);
+
+export const Drop = ({ drop, changeDrop }) => {
+  // const [{ x, y, zIndex }, api] = useSpring(() => ({
+  //   x: 0,
+  //   y: 0,
+  //   zIndex: 0,
+  // }));
+  // const bind = useGesture({
+  //   onDragStart: () => {
+  //     api.start({ zIndex: -99 });
+  //   },
+  //   onDrag: ({ down, movement: [mx, my], previous }) => {
+  //     api.start({
+  //       x: down ? mx : 0,
+  //       y: down ? my : 0,
+  //     });
+  //     const el = document.elementFromPoint(previous[0], previous[1]);
+  //     console.log(el.id);
+  //     changeDrop(drop.id, el.id);
+  //   },
+  //   onDragEnd: () => {
+  //     api.start({ zIndex: 0 });
+  //     console.log('end');
+  //   },
+  // });
+
+  const onDrag = (e) => {
+    e.preventDefault();
+    console.log(e);
+    e.dataTransfer.setData('text/plain', null);
+    e.dataTransfer.setData('text/plain', null);
+    const el = document.elementFromPoint(e.pageX, e.pageY);
+    console.log(el.id);
+    changeDrop(drop.id, el.id);
+  };
+
   return (
-    <animated.div
-      {...bind()}
-      style={{ x, y }}
-      className={`drop ${drop.color} ${drop.id}`}
+    <img
+      id={drop.id}
+      src={`sircle_d_${drop.color}.png`}
+      alt={`${drop.color}_drop`}
+      className={`drop`}
+      draggable
+      onDragStart={(e) => e.dataTransfer.setData('text/plain', null)}
+      onDrag={(e) => onDrag(e)}
     />
   );
 };
