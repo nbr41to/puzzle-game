150c7042cd69550ee05985615824e6de00ae28d9
Knob <59198478+nbr41to@users.noreply.github.com>
2023-08-15 15:40:24 +0900

    og本気

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='ja'>
-      <Head />
+      <Head>
+        <meta property='og:title' content='Puzzle and Programming' />
+        <meta property='og:type' content='website' />
+        <meta property='og:description' content='Reactでパズ◯ラ作ってみた' />
+        <meta property='og:site_name' content='Puzzle and Programming' />
+        {/* <meta property='og:image' content='' /> */}
+        <meta property='og:locale' content='ja_JP' />
+      </Head>
       <body>
         <Main />
         <NextScript />
